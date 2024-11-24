efb90147c5837ef647a37f14043a798160e7dc2e
Charlie Bacon <ct@databeacon.aero>
2024-11-24 11:20:10 +0100

    some refactor

--- src/main.zig
+++ src/main.zig
@@ -41,10 +41,9 @@ pub fn main() !void {
     }
 
     rl.setTargetFPS(60);
-    var delta = rl.getTime();
     while (!rl.windowShouldClose()) {
-        resetUpdatedParticles(&particles);
-        try updateParticles(&particles, &delta);
+        try updateParticles(&particles);
+        defer resetUpdatedParticles(&particles);
 
         rl.beginDrawing();
         defer rl.endDrawing();
@@ -79,7 +78,28 @@ pub fn main() !void {
     }
 }
 
-fn updateParticles(particles: *[GRID_WIDTH][GRID_HEIGHT]Particle, delta: *f64) !void {
+fn updateParticles(particles: *[GRID_WIDTH][GRID_HEIGHT]Particle) !void {
+    spawnNewParticles(particles);
+
+    // Update existing particles from bottom to top
+    var row: usize = GRID_HEIGHT;
+    while (row > 0) : (row -= 1) {
+        const y = row - 1;
+        var x: usize = 0;
+        while (x < GRID_WIDTH) : (x += 1) {
+            if (!particles[x][y].updated) {
+                switch (particles[x][y].kind) {
+                    .Sand => {
+                        computeSandMovement(particles, x, y);
+                    },
+                    else => {},
+                }
+            }
+        }
+    }
+}
+
+fn spawnNewParticles(particles: *[GRID_WIDTH][GRID_HEIGHT]Particle) void {
     // Spawn new sand particles with random spread
     if (rl.isMouseButtonDown(.mouse_button_left)) {
         const mouseX = rl.getMouseX();
@@ -124,72 +144,63 @@ fn updateParticles(particles: *[GRID_WIDTH][GRID_HEIGHT]Particle, delta: *f64) !
                 };
             }
         }
-        delta.* = rl.getTime();
     }
+}
 
-    // Update existing particles from bottom to top
-    var row: usize = GRID_HEIGHT;
-    while (row > 0) : (row -= 1) {
-        const y = row - 1;
-        var x: usize = 0;
-        while (x < GRID_WIDTH) : (x += 1) {
-            if (!particles[x][y].updated and particles[x][y].kind == .Sand) {
-                // Compute new particle speed because of G
-                particles[x][y].speed += @floatCast(GRAVITY * (rl.getTime() - particles[x][y].life_time));
-                if (particles[x][y].speed > MAX_SPEED) {
-                    particles[x][y].speed = MAX_SPEED;
-                }
-                const steps: usize = @intFromFloat(@round(particles[x][y].speed));
+fn computeSandMovement(particles: *[GRID_WIDTH][GRID_HEIGHT]Particle, x: usize, y: usize) void {
+    // Compute new particle speed because of G
+    particles[x][y].speed += @floatCast(GRAVITY * (rl.getTime() - particles[x][y].life_time));
+    if (particles[x][y].speed > MAX_SPEED) {
+        particles[x][y].speed = MAX_SPEED;
+    }
+    const steps: usize = @intFromFloat(@round(particles[x][y].speed));
 
-                var current_y = y;
-                for (0..steps) |step| {
-                    current_y += step;
+    var current_y = y;
+    for (0..steps) |step| {
+        current_y += step;
 
-                    // Check if we can move down
-                    if (current_y < GRID_HEIGHT - 1 and particles[x][current_y + 1].kind == .Air) {
-                        // Move down
-                        particles[x][current_y + 1] = particles[x][current_y];
-                        particles[x][current_y + 1].updated = true;
-                        particles[x][current_y] = Particle{
-                            .kind = .Air,
-                            .speed = 0,
-                            .life_time = rl.getTime(),
-                            .updated = false,
-                        };
-                    } else if (current_y < GRID_HEIGHT - 1) {
-                        // Try to move diagonally
-                        if (x > 0 and particles[x - 1][current_y + 1].kind == .Air) {
-                            // Move down-left
-                            particles[x - 1][current_y + 1] = particles[x][current_y];
-                            particles[x - 1][current_y + 1].updated = true;
-                            particles[x][current_y] = Particle{
-                                .kind = .Air,
-                                .speed = 0,
-                                .life_time = rl.getTime(),
-                                .updated = false,
-                            };
-                        } else if (x < GRID_WIDTH - 1 and particles[x + 1][current_y + 1].kind == .Air) {
-                            // Move down-right
-                            particles[x + 1][current_y + 1] = particles[x][current_y];
-                            particles[x + 1][current_y + 1].updated = true;
-                            particles[x][current_y] = Particle{
-                                .kind = .Air,
-                                .speed = 0,
-                                .life_time = rl.getTime(),
-                                .updated = false,
-                            };
-                        } else {
-                            particles[x][y].speed = 0;
-                            particles[x][y].life_time = rl.getTime();
-                            break;
-                        }
-                    } else {
-                        particles[x][y].speed = 0;
-                        particles[x][y].life_time = rl.getTime();
-                        break;
-                    }
-                }
+        // Check if we can move down
+        if (current_y < GRID_HEIGHT - 1 and particles[x][current_y + 1].kind == .Air) {
+            // Move down
+            particles[x][current_y + 1] = particles[x][current_y];
+            particles[x][current_y + 1].updated = true;
+            particles[x][current_y] = Particle{
+                .kind = .Air,
+                .speed = 0,
+                .life_time = rl.getTime(),
+                .updated = false,
+            };
+        } else if (current_y < GRID_HEIGHT - 1) {
+            // Try to move diagonally
+            if (x > 0 and particles[x - 1][current_y + 1].kind == .Air) {
+                // Move down-left
+                particles[x - 1][current_y + 1] = particles[x][current_y];
+                particles[x - 1][current_y + 1].updated = true;
+                particles[x][current_y] = Particle{
+                    .kind = .Air,
+                    .speed = 0,
+                    .life_time = rl.getTime(),
+                    .updated = false,
+                };
+            } else if (x < GRID_WIDTH - 1 and particles[x + 1][current_y + 1].kind == .Air) {
+                // Move down-right
+                particles[x + 1][current_y + 1] = particles[x][current_y];
+                particles[x + 1][current_y + 1].updated = true;
+                particles[x][current_y] = Particle{
+                    .kind = .Air,
+                    .speed = 0,
+                    .life_time = rl.getTime(),
+                    .updated = false,
+                };
+            } else {
+                particles[x][y].speed = 0;
+                particles[x][y].life_time = rl.getTime();
+                break;
             }
+        } else {
+            particles[x][y].speed = 0;
+            particles[x][y].life_time = rl.getTime();
+            break;
         }
     }
 }
